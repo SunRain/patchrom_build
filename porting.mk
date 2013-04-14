8ed9052bf4641da98841d61e9d2c48216a50a7e1
SunRain <41245110@qq.com>
2013-04-14 12:27:16 +0800

    very ugly hack to fix build break on my build env

--- porting.mk
+++ porting.mk
@@ -229,6 +229,11 @@ $(TMP_DIR)/framework-miui-res.apk: $(TMP_DIR)/framework-res.apk $(OUT_JAR_PATH)/
 	@echo "  - 3" >> $(TMP_DIR)/framework-miui-res/apktool.yml
 	@echo "  - 4" >> $(TMP_DIR)/framework-miui-res/apktool.yml
 	@echo "  - 5" >> $(TMP_DIR)/framework-miui-res/apktool.yml
+	$(hide) cat $(TMP_DIR)/framework-miui-res/apktool.yml | \
+	sed '/sdkInfo/d' | sed '/minSdkVersion/d' | \
+	sed '/targetSdkVersi/'d | sed '/packageInfo/'d | \
+	sed '/cur_package/'d | sed '/orig_package/'d | \
+	sed '/compressionType/'d > $(TMP_DIR)/framework-miui-res/apktool.yml
 	$(APKTOOL) b $(TMP_DIR)/framework-miui-res $@
 	@echo "<<< build $@ completed!"
 
